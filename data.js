48f2ffcb1ef9a87facf188fd19def533017170a6
vStudio931 <playnikita362@gmail.com>
2026-02-06 12:17:42 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const UPGRADES = [
+    { id: 'click_1', name: 'Улучшенная кирка', baseCost: 15, power: 2, type: 'click' },
+    { id: 'click_2', name: 'Алмазный бур', baseCost: 200, power: 10, type: 'click' },
+    { id: 'miner_1', name: 'Рабочий', baseCost: 50, income: 1, type: 'auto' },
+    { id: 'miner_2', name: 'Экскаватор', baseCost: 500, income: 15, type: 'auto' },
+    { id: 'miner_3', name: 'Карьерный комбайн', baseCost: 5000, income: 150, type: 'auto' }
+];
